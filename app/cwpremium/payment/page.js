b020849007320cbbedd44684df63baa0820070ec
Alexander Laptev <alex@cwr.su>
2025-03-07 23:08:45 +0300

    Updated data. Fix bug with cwpremium/payment

--- app/cwpremium/payment/page.js
+++ app/cwpremium/payment/page.js
@@ -1,31 +1,10 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import PaymentComponentPage from "./PaymentComponent";
 
 export default function PaymentPage() {
-    const router = useRouter();
-    const [searchParams, setSearchParams] = useState < URLSearchParams | null > (null);
-
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            const params = new URLSearchParams(window.location.search);
-            setSearchParams(params);
-        }
-    }, []);
-
-    useEffect(() => {
-        if (searchParams) {
-            const paymentStatus = searchParams.get("paymentStatus");
-            if (paymentStatus === "successful") {
-                localStorage.setItem("paymentStatus", "successful");
-            } else {
-                localStorage.setItem("paymentStatus", "error");
-            }
-
-            router.push("/cwpremium");
-        }
-    }, [searchParams, router]);
-
-    return null;
+    return (
+        <Suspense>
+            <PaymentComponentPage />
+        </Suspense>
+    );
 }
